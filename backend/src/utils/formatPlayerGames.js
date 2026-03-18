97d294840adc068b621c48beb1b2c6b9ff0f6301
XTK99 <traviskim99@gmail.com>
2026-03-18 03:02:09 -0700

    added more stats and team search

--- backend/src/utils/formatPlayerGames.js
+++ backend/src/utils/formatPlayerGames.js
@@ -5,12 +5,30 @@ function formatPlayerGames(games) {
     matchup: game.MATCHUP,
     result: game.WL,
     minutes: game.MIN,
+
     points: game.PTS,
     rebounds: game.REB,
+    offensiveRebounds: game.OREB,
+    defensiveRebounds: game.DREB,
     assists: game.AST,
     steals: game.STL,
     blocks: game.BLK,
     turnovers: game.TOV,
+    fouls: game.PF,
+
+    fieldGoalsMade: game.FGM,
+    fieldGoalsAttempted: game.FGA,
+    fieldGoalPct: game.FG_PCT,
+
+    threesMade: game.FG3M,
+    threesAttempted: game.FG3A,
+    threePointPct: game.FG3_PCT,
+
+    freeThrowsMade: game.FTM,
+    freeThrowsAttempted: game.FTA,
+    freeThrowPct: game.FT_PCT,
+
+    plusMinus: game.PLUS_MINUS,
   }));
 }
 
