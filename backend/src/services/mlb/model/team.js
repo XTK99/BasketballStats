d702fc80b4fae179e6ae4ccd7738f5ef1703b372
Tyler Kim <tylerkim1911@gmail.com>
2026-04-04 19:55:57 -0700

    team to class

--- backend/src/services/mlb/model/team.js
+++ backend/src/services/mlb/model/team.js
@@ -1,46 +1,45 @@
 // team.js
 // Data model for MLB teams, matching the recommended database table structure
 
-/**
- * @typedef {Object} Team
- * @property {number} id - Primary key, team ID
- * @property {string} name - Full team name
- * @property {string} team_name - Short team name
- * @property {string} abbreviation - Team abbreviation
- * @property {string} location_name - City/location
- * @property {string} short_name - Short city name
- * @property {string} franchise_name - Franchise name
- * @property {string} club_name - Club name
- * @property {string} first_year_of_play - Year team started
- * @property {boolean} active - Is the team active
- * @property {number} league_id - League ID
- * @property {number} division_id - Division ID
- * @property {number} venue_id - Venue ID
- * @property {number} season - Current season
- * @property {string} team_code - Team code
- * @property {string} file_code - File code
- */
+// Team.js
+// MLB Team model as a class
 
-/**
- * Example Team object
- */
-const exampleTeam = {
-  id: 0,
-  name: "",
-  team_name: "",
-  abbreviation: "",
-  location_name: "",
-  short_name: "",
-  franchise_name: "",
-  club_name: "",
-  first_year_of_play: "",
-  active: true,
-  league_id: 0,
-  division_id: 0,
-  venue_id: 0,
-  season: 0,
-  team_code: "",
-  file_code: "",
-};
+class Team {
+  constructor({
+    id,
+    name,
+    team_name,
+    abbreviation,
+    location_name,
+    short_name,
+    franchise_name,
+    club_name,
+    first_year_of_play,
+    active,
+    league_id,
+    division_id,
+    venue_id,
+    season,
+    team_code,
+    file_code
+  }) {
+    this.id = id;
+    this.name = name;
+    this.team_name = team_name;
+    this.abbreviation = abbreviation;
+    this.location_name = location_name;
+    this.short_name = short_name;
+    this.franchise_name = franchise_name;
+    this.club_name = club_name;
+    this.first_year_of_play = first_year_of_play;
+    this.active = !!active;
+    this.league_id = league_id;
+    this.division_id = division_id;
+    this.venue_id = venue_id;
+    this.season = season;
+    this.team_code = team_code;
+    this.file_code = file_code;
+  }
+}
 
-module.exports = { exampleTeam };
+module.exports = Team;
