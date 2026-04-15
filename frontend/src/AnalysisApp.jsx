cc45555781911f028d2e0d5685639fe4178f9d3a
Tyler Kim <tylerkim1911@gmail.com>
2026-04-14 19:21:31 -0700

    init analysis app

---
+++ frontend/src/AnalysisApp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AnalysisApp() {
+  return <h1>Hello World from AnalysisApp!</h1>;
+}
+
+export default AnalysisApp;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import NBAApp from "./NBAApp.jsx";
+import AnalysisApp from "./AnalysisApp.jsx";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NBAApp />} />
-      {/* <Route path="/analysis" element={<AnalysisApp />} /> */}
+      <Route path="/analysis" element={<AnalysisApp />} />
     </Routes>
   );
 }
