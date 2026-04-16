4e6182915b3c00e57daecd71e9d764fed4bed8ee
Tyler Kim <tylerkim1911@gmail.com>
2026-04-16 15:09:08 -0700

    MLB App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import NBAApp from "./NBAApp.jsx";
 import AnalysisApp from "./AnalysisApp.jsx";
+import MLBApp from "./MLBApp.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NBAApp />} />
       <Route path="/analysis" element={<AnalysisApp />} />
+      <Route path="/mlb" element={<MLBApp />} />
     </Routes>
   );
 }
